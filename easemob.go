b3bf2a92f6ef593c888d168a4c58c30f849d3dcb
Derek Ray <derekray@myMacdeMac.local>
2017-02-15 16:24:29 +0800

    add exported api for user

--- easemob.go
+++ easemob.go
@@ -46,3 +46,85 @@ func (em *EaseMob) GetUser(username string) (resp []byte, err error) {
 func (em *EaseMob) GetUsers(limit int, cursor string) (resp []byte, err error) {
 	return em.user.getUsers(em.context, limit, cursor)
 }
+
+func (em *EaseMob) DeleteUser(username string) error {
+	em.user.Username = username
+	_, err := em.user.delete(em.context)
+	return err
+}
+
+func (em *EaseMob) DeleteUsers(limit int) error {
+	_, err := em.user.deleteUsers(em.context, limit)
+	return err
+}
+
+func (em *EaseMob) SetPassword(username, password string) error {
+	em.user.Username = username
+	em.user.Password = password
+	_, err := em.user.setPassword(em.context)
+	return err
+}
+
+func (em *EaseMob) SetNickname(username, nickname string) error {
+	em.user.Username = username
+	em.user.Nickname = nickname
+	_, err := em.user.setNickname(em.context)
+	return err
+}
+
+func (em *EaseMob) AddFriend(username, friendname string) error {
+	em.user.Username = username
+	_, err := em.user.addFriend(em.context, friendname)
+	return err
+}
+
+func (em *EaseMob) DeleteFriend(username, friendname string) error {
+	em.user.Username = username
+	_, err := em.user.deleteFriend(em.context, friendname)
+	return err
+}
+
+func (em *EaseMob) GetFriends(username string) (data []byte, err error) {
+	em.user.Username = username
+	return em.user.getFriends(em.context)
+}
+
+func (em *EaseMob) GetBlockUsers(username string) (data []byte, err error) {
+	em.user.Username = username
+	return em.user.getBlockUsers(em.context)
+}
+
+func (em *EaseMob) BlockUsers(username string, blockusers []string) error {
+	em.user.Username = username
+	_, err := em.user.blockUsers(em.context, blockusers)
+	return err
+}
+
+func (em *EaseMob) DeleteBlockUser(username, blockuser string) error {
+	em.user.Username = username
+	_, err := em.user.deleteBlockUser(em.context, blockuser)
+	return err
+}
+
+func (em *EaseMob) GetUserStatus(username string) (data []byte, err error) {
+	em.user.Username = username
+	return em.user.getUserStatus(em.context)
+}
+
+func (em *EaseMob) Activate(username string) error {
+	em.user.Username = username
+	_, err := em.user.activate(em.context)
+	return err
+}
+
+func (em *EaseMob) Deactivate(username string) error {
+	em.user.Username = username
+	_, err := em.user.deactivate(em.context)
+	return err
+}
+
+func (em *EaseMob) Offline(username string) error {
+	em.user.Username = username
+	_, err := em.user.offline(em.context)
+	return err
+}
